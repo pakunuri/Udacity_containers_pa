c9a9008a37bbc5bbd0ddd3ff4660347c9efad01a
pakunuri <67881940+pakunuri@users.noreply.github.com>
2020-11-01 22:09:19 -0500

    incl kub files

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log('Containers rule!!!');
     console.log(`${FAVORITE_INSTRUCTOR} rocks!`);
     await sleep(5000);
   }
